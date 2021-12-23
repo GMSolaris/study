e178539f1a5354913e87755f469781a83bb9e330
Myagkikh Alexander <kvazik.vl@gmail.com>
2021-12-23 10:22:32 +1000

    fix 4.6

--- homework16 (4.2)/check_ip.py
+++ homework16 (4.2)/check_ip.py
@@ -6,8 +6,8 @@ import datetime
 
 
 i = 1
-timeout = 2 
-host_list = {'drive.google.com':'0.0.0.0', 'mail.google.com':'0.0.0.0', 'google.com':'0.0.0.0'}
+timeout = 2
+host_list = {'api.mxgroup.ru':'0.0.0.0', 'new.mxgroup.ru':'0.0.0.0', 'm.mxgroup.ru':'0.0.0.0'}
 init=0
 
 
@@ -15,7 +15,7 @@ print("Script started")
 print(host_list)
 
 
-while 1==1 : #отладочное число проверок 
+while 1==1 : 
   for host in host_list:
     ip = socket.gethostbyname(host)
     if ip != host_list[host]:
